88c351966a4ddc631edb31db0fa1fda4a0744a4d
John Whiting <john@jwhiting.dev>
2024-04-06 13:48:46 -0400

    fix: function keys weren't working as expected

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,10 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  ## Fix function key mapping
+  boot.extraModprobeConfig = ''
+    options hid_apple fnmode=0
+  '';
 
   networking.hostName = "jpc-nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
